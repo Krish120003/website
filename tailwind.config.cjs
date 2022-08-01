b678cdaed3f54c47122422b2dd113b4d485dbbd9
Krish <52761222+Krish120003@users.noreply.github.com>
2022-08-01 17:04:36 -0400

    add basic text animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,16 @@ module.exports = {
       backgroundImage: {
         texture: "url('/Texture.webp')",
       },
+      animation: {
+        "appear-bottom": "appear-bottom-keyframes 1s ease forwards",
+      },
+      keyframes: {
+        "appear-bottom-keyframes": {
+          "0%": { transform: "translateY(2ex)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animation-delay")],
 };
