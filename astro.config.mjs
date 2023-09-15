174ef0711b1aec4289e741e3ad1b8b160b9d7399
Krish120003 <krish120003@gmail.com>
2023-09-15 02:05:16 -0400

    Squashed commit of the following:
    commit c6deb375fa55b25889a133a11fcd89811200a33a
    Author: Krish120003 <krish120003@gmail.com>
    Date:   Fri Sep 15 02:04:23 2023 -0400
    
        chore: update resume
    
    commit b61449a20aa52c80ed37d4629d89e630a21d5114
    Author: Krish120003 <krish120003@gmail.com>
    Date:   Wed Jul 19 02:41:29 2023 -0400
    
        feat: add ahrefs verification
    
    commit 8403aaf72b9b72e52a446ce2176f97c8f1223c47
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Fri Apr 28 14:44:09 2023 -0400
    
        Update Resume.pdf
    
    commit 6dbdba5dc3b351691720c8c3ef4a65dcd6d18d51
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Sat Mar 25 17:58:45 2023 -0400
    
        chore: add more description for river
    
    commit 62cc3a3dfd463ec34e8037a898990196678749a4
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Sat Mar 25 17:50:22 2023 -0400
    
        fix: change year
    
    commit 7bfc3cdbac7b17bf2d57409938e34b5837d6c672
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Sat Mar 25 17:45:23 2023 -0400
    
        feat: add cool pattern at the bottom
    
    commit 039950d1c5e8ed669ab2bb43279f0c2e3da14187
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Thu Mar 23 13:04:27 2023 -0400
    
        fix: replace bad grain with simple gradient
    
    commit e58d1c19ad1ee87af24c524843db3d72f178f51a
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Thu Mar 23 13:01:52 2023 -0400
    
        fix: change image formats to webp
    
    commit 16b4e8cdcc95d2a71a7c3a0fb7b16c93cacafe66
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 21:12:08 2023 -0400
    
        feat: reorganize
    
    commit f448009edcc36e0606a98c82036628385b472437
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 20:11:36 2023 -0400
    
        feat: add projects to index
    
    commit 3e62f0ec0c6f8cb7987049dc836c9d09659c8a0c
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 19:59:34 2023 -0400
    
        move project data
    
    commit 0a80c4299bd2c8e4f0b199cfbf36635ac78cc012
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 19:47:00 2023 -0400
    
        feat: add temp projects page
    
    commit 468a1ffbb14f4252a054c7c0cf81c46b4a1b48fe
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 19:46:34 2023 -0400
    
        Crate project component
    
    commit c78ae188b819985e6cd983d6087a4114c19f4f33
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 19:46:11 2023 -0400
    
        feat: add @astro/image
    
    commit 5af39cc3c2ed69ae74c09f3f6be5d7f79f727d6f
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 14:17:46 2023 -0400
    
        move to header component
    
    commit c8f4c87da4c3e5b5a07f1ca4492b6bcb8a9103c9
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 14:17:35 2023 -0400
    
        Create env.d.ts
    
    commit 6364fc14d19ee786beaa8ec66dca3cf004da4c5f
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 14:08:34 2023 -0400
    
        Create Krish_Krish_GCP_ACE.pdf
    
    commit ef89aafcb401e9978e62990b247f559768f7cb94
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Wed Mar 22 14:01:38 2023 -0400
    
        upgrade to 2.0, install react
    
    commit 1622d12aec5f55010482b89aafa807232f7dfa35
    Author: Krish <52761222+Krish120003@users.noreply.github.com>
    Date:   Mon Jan 9 11:48:30 2023 -0500
    
        Update Resume.pdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"; // https://astro.build/config
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()]
 });
