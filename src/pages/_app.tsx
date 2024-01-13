cb110059202105cf942c51b2c842977ee2043038
Krish120003 <krish120003@gmail.com>
2024-01-13 03:38:35 -0500

    feat: add logrocket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
 
 import "~/styles/globals.css";
+import { useEffect } from "react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    LogRocket.init("mg25jh/personal-website");
+
+    setupLogRocketReact(LogRocket);
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
