78151bf7762e0531dad66699561069d107558862
Krish120003 <krish120003@gmail.com>
2024-01-15 12:55:48 -0500

    chore: hide blog until more post

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,13 @@ import setupLogRocketReact from "logrocket-react";
 import "~/styles/globals.css";
 import { useEffect } from "react";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   useEffect(() => {
+    if (!isProd) {
+      return;
+    }
     LogRocket.init("mg25jh/personal-website");
 
     setupLogRocketReact(LogRocket);
