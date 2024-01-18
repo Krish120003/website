7bdc5d73b96d4651a8806a5849dea9230f0cc427
Krish120003 <krish120003@gmail.com>
2024-01-18 17:52:42 -0500

    feat: add blog og

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,160 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+const interRegular = fetch(
+  new URL("../../../public/fonts/Inter-Regular.ttf", import.meta.url),
+).then((res) => res.arrayBuffer());
+
+const interBold = fetch(
+  new URL("../../../public/fonts/Inter-Bold.ttf", import.meta.url),
+).then((res) => res.arrayBuffer());
+
+export default async function handler(request: NextRequest) {
+  try {
+    const [interRegularD, interBoldD] = await Promise.all([
+      interRegular,
+      interBold,
+    ]);
+
+    const { searchParams } = new URL(request.url);
+
+    // ?title=<title>
+    const hasTitle = searchParams.has("title");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "krish's blog";
+
+    const hasPublishTime = searchParams.has("publishTime");
+    const publishTime = hasPublishTime
+      ? searchParams.get("publishTime")?.slice(0, 100)
+      : "";
+
+    const hasReadingTime = searchParams.has("readingTime");
+    const readingTime = hasReadingTime
+      ? searchParams.get("readingTime")?.slice(0, 100)
+      : "";
+
+    const hasDescription = searchParams.has("description");
+    const description = hasDescription
+      ? searchParams.get("description")?.slice(0, 400)
+      : "";
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: "#171717",
+            backgroundSize: "150px 150px",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "left",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            padding: "4em",
+          }}
+        >
+          <div
+            style={{
+              //   backgroundColor: "#ff0000",
+              display: "flex",
+              flexDirection: "column",
+              //   alignItems: "center",
+              //   justifyContent: "center",
+              //   justifyItems: "center",
+              color: "white",
+            }}
+          >
+            <h1
+              style={{
+                // fontWeight: "bold",
+                fontSize: "4em",
+                fontFamily: "sans-serif",
+                margin: "0",
+              }}
+            >
+              {title}
+            </h1>
+            <p
+              style={{
+                fontSize: "2em",
+                opacity: 0.45,
+              }}
+            >
+              {description}
+            </p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <p
+              style={{
+                margin: "0",
+                fontFamily: "sans-serif",
+                color: "white",
+                opacity: 0.75,
+                fontSize: "1.5em",
+              }}
+            >
+              {publishTime}
+            </p>
+            <p
+              style={{
+                margin: "0",
+                fontFamily: "sans-serif",
+                color: "white",
+                opacity: 0.75,
+                fontSize: "1.5em",
+              }}
+            >
+              {readingTime} minute read
+            </p>
+            <p
+              style={{
+                margin: "0",
+                fontFamily: "sans-serif",
+                color: "white",
+                opacity: 0.75,
+                fontSize: "1.5em",
+              }}
+            >
+              krish's blog • krishkrish.com
+            </p>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: "Inter",
+            data: interRegularD,
+            style: "normal",
+            weight: 400,
+          },
+          {
+            name: "Inter",
+            data: interBoldD,
+            style: "normal",
+            weight: 700,
+          },
+        ],
+      },
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -4,12 +4,45 @@ import type { InferGetStaticPropsType } from "next";
 import { Layout } from "~/components/Layout";
 import { format } from "date-fns";
 export default function Post({
-  postData: { title, date, contentHtml, readTime },
+  postData: { title, date, contentHtml, readTime, description, id },
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const publishDateFormatted = format(date, "LLL d, y");
+  const hostUrl = "https://krishkrish.com";
+  // const hostUrl = "http://localhost:3000";
+
+  const escapedTitle = encodeURIComponent(title);
+  const escapedDescription = encodeURIComponent(description);
+  const escapedPublishDate = encodeURIComponent(publishDateFormatted);
+  const escapedReadTime = encodeURIComponent(readTime);
+
+  const ogImageUrl = `${hostUrl}/api/og?title=${escapedTitle}&description=${escapedDescription}&publishTime=${escapedPublishDate}&readingTime=${escapedReadTime}"`;
+
   return (
     <>
       <Head>
         <title>{`krish's blog • ${title}`}</title>
+        <meta property="og:title" content={`krish's blog • ${title}`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={`https://krishkrish.com/blog/${id}`} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="krishkrish.com" />
+        <meta
+          property="twitter:url"
+          content={`https://krishkrish.com/blog/${id}`}
+        />
+        <meta property="twitter:description" content={description} />
+
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* meta images */}
+        <meta property="og:image" content={ogImageUrl} />
+        <meta property="twitter:image" content={ogImageUrl} />
       </Head>
       <Layout blog>
         <div className="">
@@ -17,7 +50,7 @@ export default function Post({
             {title}
           </h1>
           <div className="py-1 text-sm dark:opacity-80">
-            Published {format(date, "LLL d, y")} • {readTime} minute read
+            Published {publishDateFormatted} • {readTime} minute read 
           </div>
 
           <div
