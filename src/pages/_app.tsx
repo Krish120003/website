788986feafbb45925e54a5de8b66d9024103bcdd
Krish120003 <krish120003@gmail.com>
2024-04-04 04:26:38 -0400

    feat: add posthog analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,24 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import "~/styles/globals.css";
 import { useEffect } from "react";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
 import "prismjs/themes/prism-okaidia.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import { env } from "~/env";
 
 const isProd = process.env.NODE_ENV === "production";
+
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   useEffect(() => {
@@ -23,7 +37,9 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <SpeedInsights />
-      <Component {...pageProps} />
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
     </>
   );
 };
