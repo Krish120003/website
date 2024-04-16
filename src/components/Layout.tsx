f15498a9aa928a8de65a93a1750919f4c70e9cdc
Krish120003 <krish120003@gmail.com>
2024-04-16 03:41:46 -0400

    style: adjust scroll to clip instead of transform
    now the gradient actually works

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ const ScrollIndicator = () => {
   const handleScroll = () => {
     const scroll = window.scrollY;
     const height = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = (scroll / height) * 100;
+    const scrolled = 100 - (scroll / height) * 100;
     document.documentElement.style.setProperty("--scroll", `${scrolled}%`);
   };
 
@@ -24,10 +24,9 @@ const ScrollIndicator = () => {
 
   return (
     <div
-      className="fixed right-0 w-1 h-full origin-top transform-gpu bg-gradient-to-b from-neutral-800 to-neutral-950 md:left-0 dark:from-orange-50 dark:to-orange-200"
+      className="fixed right-0 h-full w-1 origin-top transform-gpu bg-gradient-to-b from-neutral-800 to-neutral-950 md:left-0 dark:from-orange-50 dark:to-sky-400"
       style={{
-        transform: "scaleY(var(--scroll))",
-        transition: "transform 0.01s",
+        clipPath: "inset(0 0 var(--scroll) 0)",
       }}
     ></div>
   );
@@ -42,19 +41,19 @@ export const Layout: React.FC<LayoutProps> = ({ children, blog }) => {
   return (
     <>
       <ScrollIndicator />
-      <div className="font-serif h-fit dark:text-white">
-        <main className="max-w-2xl min-h-full px-12 pt-12 m-auto">
+      <div className="h-fit font-serif dark:text-white">
+        <main className="m-auto min-h-full max-w-2xl px-12 pt-12">
           {blog ? (
             <Link
               href={parentRoute}
-              className="flex items-center mb-8 gap-1 opacity-60 transition-all hover:gap-2 hover:opacity-90"
+              className="mb-8 flex items-center gap-1 opacity-60 transition-all hover:gap-2 hover:opacity-90"
             >
               <IoMdArrowBack />
               Back
             </Link>
           ) : null}
           {children}
-          <footer className="flex flex-col justify-between py-8 mt-8 border-t border-black opacity-20 transition-opacity hover:opacity-50 md:flex-row dark:border-white">
+          <footer className="mt-8 flex flex-col justify-between border-t border-black py-8 opacity-20 transition-opacity hover:opacity-50 md:flex-row dark:border-white">
             <div>&copy; {new Date().getFullYear()} Krish Krish</div>
             <Link href="mailto:hello@krishkrish.com">hello@krishkrish.com</Link>
           </footer>
