ad044fa9dbcd27a7ad4dda40d04a85dfe3a3b776
Krish120003 <krish120003@gmail.com>
2024-04-17 06:12:39 -0400

    feat: new blog post header, table of content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { IoMdArrowBack } from "react-icons/io";
+import { cn } from "~/lib/utils";
 
 interface LayoutProps {
   children: React.ReactNode | React.ReactNode[] | string;
+  back?: boolean;
   blog?: boolean;
 }
 
@@ -12,8 +14,13 @@ const ScrollIndicator = () => {
   const handleScroll = () => {
     const scroll = window.scrollY;
     const height = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = 100 - (scroll / height) * 100;
-    document.documentElement.style.setProperty("--scroll", `${scrolled}%`);
+    const scrolled = (scroll / height) * 100;
+    const inverseScrolled = 100 - scrolled;
+    document.documentElement.style.setProperty("--scroll", `${scrolled}`);
+    document.documentElement.style.setProperty(
+      "--scroll-indicator",
+      `${inverseScrolled}%`,
+    );
   };
 
   useEffect(() => {
@@ -26,13 +33,13 @@ const ScrollIndicator = () => {
     <div
       className="fixed  h-full w-1 origin-top transform-gpu bg-gradient-to-b from-neutral-800 to-neutral-950  dark:from-orange-50 dark:to-sky-400"
       style={{
-        clipPath: "inset(0 0 var(--scroll) 0)",
+        clipPath: "inset(0 0 var(--scroll-indicator) 0)",
       }}
     ></div>
   );
 };
 
-export const Layout: React.FC<LayoutProps> = ({ children, blog }) => {
+export const Layout: React.FC<LayoutProps> = ({ children, blog, back }) => {
   const router = useRouter();
   // get the parent route, so we can use it to go back
   const parentRoute =
@@ -42,8 +49,13 @@ export const Layout: React.FC<LayoutProps> = ({ children, blog }) => {
     <>
       <ScrollIndicator />
       <div className="h-fit font-serif dark:text-white">
-        <main className="m-auto min-h-full max-w-2xl px-12 pt-12">
-          {blog ? (
+        <main
+          className={cn("m-auto min-h-full px-8 pt-12 md:px-0", {
+            "max-w-2xl ": !blog,
+            "md:px-24": blog,
+          })}
+        >
+          {back ? (
             <Link
               href={parentRoute}
               className="mb-8 flex items-center gap-1 opacity-60 transition-all hover:gap-2 hover:opacity-90"
--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -3,8 +3,37 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import type { InferGetStaticPropsType } from "next";
 import { Layout } from "~/components/Layout";
 import { format } from "date-fns";
+import { FaArrowUp } from "react-icons/fa";
+
+const ScrollToTopButton = () => {
+  // use the --scroll custom property to show/hide the button
+
+  return (
+    <button
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+      className="fixed bottom-8 right-8 rounded-md bg-neutral-100 p-2 text-2xl shadow-md dark:bg-slate-800 dark:text-white dark:shadow-lg"
+      style={{
+        // use clever calc, if scroll is 0, opacity is 0, if scroll is more than 0.01, opacity is 1
+        opacity: "var(--scroll)",
+      }}
+    >
+      <FaArrowUp />
+    </button>
+  );
+};
+
 export default function Post({
-  postData: { title, date, contentHtml, readTime, description, id },
+  postData: {
+    title,
+    date,
+    contentHtml,
+    readTime,
+    description,
+    id,
+    tableOfContents,
+  },
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const publishDateFormatted = format(date, "LLL d, y");
   const hostUrl = "https://krishkrish.com";
@@ -44,20 +73,33 @@ export default function Post({
         <meta property="og:image" content={ogImageUrl} />
         <meta property="twitter:image" content={ogImageUrl} />
       </Head>
-      <Layout blog>
-        <div className="">
-          <h1 className="text-3xl font-semibold leading-tight tracking-tight font-serif-condensed">
-            {title}
-          </h1>
-          <div className="py-1 text-sm font-serif-condensed dark:opacity-80">
-            Published {publishDateFormatted} • {readTime} minute read 
+      {/* <ScrollToTopButton /> */}
+      <Layout blog back>
+        <div className="grid grid-cols-1 gap-8 py-12 md:grid-cols-2 md:gap-0">
+          <div className="md:pr-16">
+            <h1 className="text-balance font-serif-condensed text-3xl font-semibold leading-tight tracking-tight md:text-5xl">
+              {title}
+            </h1>
+            <div className="py-1 font-serif text-sm dark:opacity-80">
+              Published {publishDateFormatted} • {readTime} minute read 
+            </div>
           </div>
 
-          <div
-            dangerouslySetInnerHTML={{ __html: contentHtml }}
-            className="prose-md prose prose-neutral w-full max-w-none pt-8 dark:prose-invert [&>p>img]:m-auto"
-          />
+          <aside className="md:pl-12">
+            <span className="font-serif opacity-70">Table of Contents</span>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: tableOfContents,
+              }}
+            ></div>
+          </aside>
         </div>
+        <hr className="m-auto  border-neutral-400 py-4 dark:border-white dark:opacity-10" />
+
+        <div
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+          className="prose-md prose prose-neutral m-auto w-full max-w-2xl dark:prose-invert [&>p>img]:m-auto"
+        />
       </Layout>
     </>
   );
