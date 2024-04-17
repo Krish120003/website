558d8638d3cec743b0b3e4dbfb10d3542cdb45ed
Krish120003 <krish120003@gmail.com>
2024-04-17 03:39:53 -0400

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+  siteUrl: process.env.SITE_URL || "https://krishkrish.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
