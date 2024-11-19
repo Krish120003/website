8bdadbc6adbdc6a0e05f7545d4ee7ba4dfe05c7d
Krish120003 <krish120003@gmail.com>
2024-11-19 02:40:37 -0500

    fix: move head from _doc to _app
    https://nextjs.org/docs/messages/no-document-viewport-meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { PostHogProvider } from "posthog-js/react";
 import "prismjs/themes/prism-okaidia.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import { env } from "~/env";
+import Head from "next/head";
 
 if (typeof window !== "undefined") {
   // checks that we are client-side
@@ -25,6 +26,32 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <>
       <SpeedInsights />
       <PostHogProvider client={posthog}>
+        <Head>
+          <title>krish&apos;s website</title>
+          <meta name="description" content="krish's personal website" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.krish.gg" />
+          <meta property="og:title" content="krish's personal website" />
+
+          <meta property="og:description" content="krish's personal website" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="www.krish.gg" />
+          <meta property="twitter:url" content="https://www.krish.gg" />
+          <meta name="twitter:title" content="krish's personal website" />
+          <meta name="twitter:description" content="krish's personal website" />
+
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <meta name="viewport" content="width=device-width" />
+
+          {/* meta images */}
+          <meta property="og:image" content="https://www.krish.gg/og.jpg" />
+          <meta name="twitter:image" content="https://www.krish.gg/og.jpg" />
+
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </PostHogProvider>
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,32 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>krish&apos;s website</title>
-        <meta name="description" content="krish's personal website" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.krish.gg" />
-        <meta property="og:title" content="krish's personal website" />
-
-        <meta property="og:description" content="krish's personal website" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="www.krish.gg" />
-        <meta property="twitter:url" content="https://www.krish.gg" />
-        <meta name="twitter:title" content="krish's personal website" />
-        <meta name="twitter:description" content="krish's personal website" />
-
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
-        <meta name="viewport" content="width=device-width" />
-
-        {/* meta images */}
-        <meta property="og:image" content="https://www.krish.gg/og.jpg" />
-        <meta name="twitter:image" content="https://www.krish.gg/og.jpg" />
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body className="bg-neutral-100 dark:bg-neutral-950">
         <Main />
         <NextScript />
