a3e37eb089df57cee7c9f0d05b62f811d763f00e
Krish120003 <krish120003@gmail.com>
2025-03-11 02:16:47 -0400

    feat: light mode is lighter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="[*>]:font-sans bg-neutral-100 dark:bg-[#101010]">
+      <body className="[*>]:font-sans bg-neutral-50 dark:bg-[#101010]">
         <Main />
         <NextScript />
       </body>
