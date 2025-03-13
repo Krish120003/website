d07be759b56d44e7be97286d6fae5b8a6d20918b
Krish120003 <krish120003@gmail.com>
2025-03-12 20:07:41 -0400

    blog: so I rebuilt Vercel...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx", "./src/**/*.ts"],
+  content: ["./src/**/*.tsx", "./src/**/*.ts", "posts/**/*.md"],
   theme: {
     extend: {
       animationDelay: {
